07efe259124fc922a4faaf866c5ca5a59854816a
FOODpanda <jamesanthonyvillacorta1@gmail.com>
2025-04-03 18:31:36 +0800

    Update studentsa.java

--- mysql/src/mysql/studentsa.java
+++ mysql/src/mysql/studentsa.java
@@ -12,10 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/**
- *
- * @author YANGYANG_CPE121
- */
+
 public class studentsa extends javax.swing.JFrame {
 
      
